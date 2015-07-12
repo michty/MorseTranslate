0ab796e1a909f2656b27032a3ae7f4e008ce4b88
Michal Tyrlik <misatyrlik@Michals-Mac.local>
2015-07-12 08:04:37 -0600

    Initial Commit

---
+++ MorseTranslate/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MorseTranslate
+//
+//  Created by Michal Tyrlik on 7/12/15.
+//  Copyright (c) 2015 Michal Tyrlik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
